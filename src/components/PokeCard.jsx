c6663c9b0f57fa39874e3b28892cca219b75b984
elkingutierrex <elkin.gutierrex@gmail.com>
2024-08-21 18:37:10 -0500

    feat : se agrega duncionalidad para visualizar pokemones

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,10 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+import { Col, Card, CardBody, CardFooter, CardImg, Badge } from "reactstrap" 
 
+export const PokeCard = ( params ) => {
+  const [ pokemon, setPokemon ] = useState([]);
+  const [ imagen, setImagen ] = useState('');
+  const [ cardClass, setCardClass ] = useState('d-none');
+  const [ loadClass, setLoadClass ] = useState('');
 
-export const PokeCard = () => {
-  return (
-    <div>
+  useEffect(()=>{
+    getPokemon()
+  },[])
+
+  const getPokemon = async(  ) => {
+      const url= params.pokemon.url;
+      axios.get(url).then( async(response) => {
+        const resp = response.data;
+        setPokemon(resp);
+        if(resp.sprites.other.dream_world.front_default != null){
+          setImagen(resp.sprites.other.dream_world.front_default);
+        }else{
+          setImagen(resp.sprites.other['official-artwork'].front_default)
+        }
+        setCardClass('');
+        setLoadClass('d-none')
+      })
       
-    </div>
+    }
+  
+  return (
+    <>
+    
+      <Col sm="4" lg="3" className="mb-3">
+
+        <Card className={"shadow border-4 border-warning "+ loadClass}>
+          <CardImg src="../../public/img/pokebola.gif" width="50"  className="p-3">
+
+          </CardImg>
+        </Card>
+
+        <Card className={"shadow border-4 border-warning "+ cardClass}>
+          <CardImg src={imagen} height="150" className="p-2"/>
+            <CardBody className="text-center">
+              <Badge pill color="danger"> # {pokemon.id} </Badge>
+              <label className="fs-4 text-capitalize"> {pokemon.name} </label>
+            </CardBody>
+
+            <CardFooter className="bg-warning">
+              <Link className="btn btn-dark">
+                <i className="fa-solid fa-arrow-up-right-from-square"> </i> Detalle 
+              </Link>
+            </CardFooter>
+
+          
+        </Card>
+      </Col>
+      
+    </>
   )
 }
 
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,9 +1,49 @@
+import { Container, Row, Col, InputGroup, InputGroupText, Input } from "reactstrap";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { PokeCard } from "../components/PokeCard";
 
 export const Index = () => {
+  const [ pokemons, setPokemons ] = useState([]);
+  const [ offSet, setOffset ] = useState(0);
+  const [ limit, setLimit ] = useState(20);
+
+  useEffect(() => {
+    getPokemons(offSet);
+  }, [offSet]); // Puedes agregar `offSet` como dependencia si esperas que cambie
+
+  const getPokemons = async (startNumber) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offSet}`;
+    axios.get(url).then(async (response) => {
+      const resp = response.data.results;
+      setPokemons(resp)
+      console.log(resp);
+    });
+  };
+
   return (
     <>
-      <h1>Index</h1>
-    </>
-  )
-}
+      <Container className="shadow bg-danger mt-3 mb-3 p-2">
+        <Row>
+          <Col>
+            <InputGroup className="shadow">
+              <InputGroupText>
+                <i className="fa-solid fa-search"></i>
+              </InputGroupText>
+              <Input placeholder="Buscar Pokemon"></Input>
+            </InputGroup>
+          </Col>
+        </Row>
 
+        <Row className="mt-3">
+          {
+            pokemons.map((pokemon, i) => (
+              <PokeCard key={i} pokemon={pokemon} />
+       
+            )
+          )}
+        </Row>
+      </Container>
+    </>
+  );
+};
